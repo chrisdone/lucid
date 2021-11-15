3f19b845077ba6dafe26cddf5598e6d718bb78f8
Chris Done <chrisdone@gmail.com>
2021-11-15 16:15:34 +0000

    Delete .dir-locals.el

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-((haskell-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-path-ghci . "stack")
-     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng"))))
- (haskell-cabal-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-path-ghci . "stack")
-     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng")))))
